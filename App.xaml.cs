f527032f916ca55a30f1cb6399dbeefacca6414e
George <vaidageorge290@gmail.com>
2025-11-26 12:04:03 +0200

    lab8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,25 @@
-﻿namespace VaidaGeorgeLab7
+﻿using System;
+using VaidaGeorgeLab7.Data;
+using System.IO;
+
+namespace VaidaGeorgeLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
