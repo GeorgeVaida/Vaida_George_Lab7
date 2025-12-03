dccb79f72b2b81e032ea4d3dd6619ab138859096
George <George@DESKTOP-MR85PBG>
2025-12-03 10:09:57 +0200

    lab9

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -8,6 +8,29 @@ public partial class ListPage : ContentPage
 	{
 		InitializeComponent();
 	}
+    
+    async void OnDeleteItemClicked(object sender, EventArgs e)
+    {
+        var product = listView.SelectedItem as Product;
+        await App.Database.DeleteProductAsync(product);
+        listView.ItemsSource = await App.Database.GetProductsAsync();
+    }
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+       this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
     async void OnSaveButtonClicked(object sender, EventArgs e)
     {
         var slist = (ShopList)BindingContext;
